1eb8f43dbcf549accd17ad15aafd9e31de7c2386
oaao <oliver+github@abolish.golf>
2026-05-14 18:25:08 -0600

    add [01 - js review]: ex01 - set up DOM object & data import for rendering expense cards

--- 01 - javascript review/examples/01 - expense tracker - basic review/instructor/js/main.js
+++ 01 - javascript review/examples/01 - expense tracker - basic review/instructor/js/main.js
@@ -1,1 +1,13 @@
-//Instruction On expense-tracker-code-explained.md
+// 1. import the data from the file (in lieu of e.g. a database / REST API)
+import expenses from './expense-data.js';
+
+// 2. grab relevant DOM elements
+const expenseContainer = document.getElementById('expense-container')
+
+// 3. [skeleton]: render out data into a grid of cards
+function renderExpenses(expenseData) {
+    
+}
+
+// 4. call the function to actually do the render
+renderExpenses(expenses);
