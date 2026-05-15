e579bc0732eb3d7653be70338ec862f842ee2d4d
Tyler hatch <124322813+DrPeepPeep@users.noreply.github.com>
2026-05-14 18:24:12 -0600

    js skeleton setup

--- 01 - javascript review/examples/01 - expense tracker - basic review/student/js/main.js
+++ 01 - javascript review/examples/01 - expense tracker - basic review/student/js/main.js
@@ -1,1 +1,14 @@
 //Instruction On expense-tracker-code-explained.md
+//1 import the data from the file (in lieu of e.g a database / REST API)
+import expenses from "./expense-data.js";
+
+//2 grab relevant DOM elements
+const expenseContainer = document.getElementById("expense-container");
+
+//3 [Skeleton] render out data into a grid of cards
+function renderExpenses(expensesData) {
+    console.log(expensesData);
+}
+
+//4 call the function to render the data on page load
+renderExpenses(expenses);
