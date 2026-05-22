e46b4217fd5f759818da1c262e079be64fc4df1c
Tyler hatch <124322813+DrPeepPeep@users.noreply.github.com>
2026-05-21 18:15:16 -0600

    add [01 - js review]: ex01 - skeleton for adding expense via form

--- 01 - javascript review/examples/01 - expense tracker - basic review/student/js/main.js
+++ 01 - javascript review/examples/01 - expense tracker - basic review/student/js/main.js
@@ -5,17 +5,18 @@ import expenses from "./expense-data.js";
 //2 grab relevant DOM elements
 const expenseContainer = document.getElementById("expense-container");
 const searchTerm = document.getElementById("searchbox");
+const expenseForm = document.getElementById("expense-form-add");
 
-//3 [Skeleton] render out data into a grid of cards
+//3(a) [Skeleton] render out data into a grid of cards
 function renderExpenses(expensesData) {
     //first, clear out existing HTML for the container (because we're about to rerender it)
     expenseContainer.innerHTML = "";
+
     //then, take our array of data and render out a card for each one
     expensesData.forEach(
         //for a given expense, add a new card containing that data to the expenseContainer's inner HTML
         (expense) => {
             expenseContainer.innerHTML += `
-                    
                         <div class="card" id="">
                             <div class="header">
                                 <div>
@@ -30,7 +31,6 @@ function renderExpenses(expensesData) {
                                 <button class="delete-btn" id="${expense.id}">Delete</button>
                             </div>
                         </div>
-                    
         `;
         },
     );
@@ -38,3 +38,10 @@ function renderExpenses(expensesData) {
 
 //4 call the function to render the data on page load
 renderExpenses(expenses);
+
+//5  [skeleton]: form submission
+function submitExpense() {
+    // ...
+}
+
+expenseForm.addEventListener("submit", submitExpense);
