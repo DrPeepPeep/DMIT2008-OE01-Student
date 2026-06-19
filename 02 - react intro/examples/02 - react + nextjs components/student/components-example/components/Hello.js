887b4e1adf0e6a1422c3e883fcc2926d6a00582b
Tyler hatch <124322813+DrPeepPeep@users.noreply.github.com>
2026-06-18 18:42:56 -0600

    Components with props

---
+++ 02 - react intro/examples/02 - react + nextjs components/student/components-example/components/Hello.js
@@ -0,0 +1,4 @@
+//create a function named Hello with export default before the function keyword.
+export default function Hello() {
+  return <p>This is my first component.</p>;
+}
---
+++ 02 - react intro/examples/02 - react + nextjs components/student/components-example/components/NewConcept.js
@@ -0,0 +1,17 @@
+// props short for properties, are the standard way of passing data
+// between components, and generally look like HTML attributes.
+
+// There are two ways of ingesting props:
+// a) one single argument for all props > each prop is a property on the object
+// b) destructuring > each prop directly (eg {concept, author})
+
+// you'd use this in JSX as:
+// <NewConcept concept="How to make a component" author="Tyler" />
+
+export default function NewConcept({ concept, author }) {
+  return (
+    <p>
+      In this class we'll learn: {concept} by {author}
+    </p>
+  );
+}
