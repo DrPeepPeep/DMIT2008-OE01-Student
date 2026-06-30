428836be05fb510dda2e2187621fa9b4a4eb89be
Tyler hatch <124322813+DrPeepPeep@users.noreply.github.com>
2026-06-29 18:43:42 -0600

    Set state into our todolist.js using terms todoItem & settodoItem

--- 02 - react intro/examples/05 - state/student/state-events-fundamental-example/components/TodoList.js
+++ 02 - react intro/examples/05 - state/student/state-events-fundamental-example/components/TodoList.js
@@ -1,5 +1,17 @@
+//State lest us store persistent data across component re-renders, and automate re-rendering whenver that data changes.
+
+import { useState } from "react";
 import TextField from "@mui/material/TextField";
 
 export default function TodoList() {
-  return <TextField id="standard-basic" label="New todo item" variant="standard" sx={{ width: "100%" }} />;
+  // First term: The variable that will hold the value
+  // Second term: The function that will update the value (is the only thing able to change the value)
+  const [todoItem, settodoItem] = useState("");
+
+  const onTodoTextChange = (event) => {
+    // call state variables setter w/ new value to write
+    settodoItem(event.target.value);
+  };
+
+  return <TextField id="standard-basic" label="New todo item" variant="standard" sx={{ width: "100%" }} value={todoItem} onChange={onTodoTextChange} />;
 }
