99b2085e28903ea079a569b12e301537496a8b12
Tyler hatch <124322813+DrPeepPeep@users.noreply.github.com>
2026-08-06 18:33:41 -0600

    populating page with API data

--- 05 - routing/example - spaaaace/student/pages/agency/[agencyId].js
+++ 05 - routing/example - spaaaace/student/pages/agency/[agencyId].js
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
 import { useRouter } from "next/router";
 
@@ -33,26 +32,57 @@ export default function Agency() {
   return (
     <>
       <NavBar />
-      <Container sx={{ paddingTop: 2 }}>
-        <Grid container>
-          {!agencyDetails ? (
-            <LoadingCircle />
-          ) : (
-            <Container sx={{ paddingTop: 2 }}>
-              <Grid container>
-                <Grid item xs="2">
-                  <img alt={agencyDetails.name} src={agencyDetails.logo_url} style={{ width: "120px" }} />
-                </Grid>
-                <Grid item xs="10">
-                  <Typography variant="h3" gutterBottom>
-                    Agency Page for {agencyId}
-                  </Typography>
-                </Grid>
-              </Grid>
-            </Container>
-          )}
-        </Grid>
-      </Container>
+      {!agencyDetails ? (
+        <LoadingCircle />
+      ) : (
+        <Container sx={{ paddingTop: 2 }}>
+          <Grid container>
+            <Grid item xs="2">
+              <img alt={agencyDetails.name} src={agencyDetails.logo_url} style={{ width: "120px" }} />
+            </Grid>
+
+            <Grid item xs="10">
+              <Typography variant="h3" gutterBottom>
+                {`${agencyDetails.name} (${agencyDetails.abbrev})`}
+              </Typography>
+            </Grid>
+
+            <Grid item xs="4" my={2}>
+              <Typography variant="h5">{"Launch Details"}</Typography>
+              <SimpleDetailsCard title={"Total Launches"} description={agencyDetails.total_launch_count} />
+              <SimpleDetailsCard title={"Successful Launches"} description={agencyDetails.successful_launches} />
+              <SimpleDetailsCard title={"Successful Landings"} description={`${agencyDetails.successful_landings}`} />
+            </Grid>
+
+            <Grid item xs="4" my={2}>
+              <Typography variant="h5">{`Agency Information`}</Typography>
+              <SimpleDetailsCard title={"administrator"} description={`${agencyDetails.administrator}`} />
+              <SimpleDetailsCard
+                title={"Space Agency Details"}
+                description={`Founded ${agencyDetails.founding_year}`}
+                subDescription={agencyDetails.description}
+              />
+            </Grid>
+
+            <Grid item xs="4">
+              <Typography variant="h5">{`SpaceCraft`}</Typography>
+              {agencyDetails.spacecraft_list &&
+                agencyDetails.spacecraft_list.map((spaceCraft) => {
+                  return (
+                    <SimpleDetailsCard
+                      key={spaceCraft.id}
+                      description={`${spaceCraft.name}`}
+                      buttonCallback={() => {
+                        console.log("go to the space page");
+                      }}
+                      buttonName="Go to SpaceCraft"
+                    />
+                  );
+                })}
+            </Grid>
+          </Grid>
+        </Container>
+      )}
     </>
   );
 }
